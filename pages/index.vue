a6b44d994a72f7a6d8968a6df80c2a0bf1dcb6f7
Andrey Buchnev <andrey@scada.lv>
2020-11-10 18:09:20 +0200

    update styles

--- pages/index.vue
+++ pages/index.vue
@@ -11,16 +11,3 @@
 <script>
 export default {}
 </script>
-
-<style lang="scss" scoped>
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 70px;
-  color: #35495e;
-  letter-spacing: 1px;
-  margin-bottom: 32px;
-}
-</style>
